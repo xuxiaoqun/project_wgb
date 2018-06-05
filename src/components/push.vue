cbc4211830f01dd92c208036429aeb4624262dac
wgb <wgb@nefu.edu.cn>
2018-06-05 17:15:03 +0800

    代码样式优化

---
+++ src/components/push.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="like" >
+      <el-form ref="form" :model="form" label-width="150px">
+        <el-form-item  label="被推送会员">
+          <el-input v-model="form.title" style="width: 70%;">
+            <el-button slot="append" icon="el-icon-plus"></el-button>
+          </el-input>
+        </el-form-item>
+        <el-form-item  label="被推送标签">
+          <el-input v-model="form.title" style="width: 70%;">
+            <el-button slot="append" icon="el-icon-plus"></el-button>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="推送信息">
+          <el-input type="textarea" v-model="form.desc"
+                    rows="10" style="width: 70%;">
+          </el-input>
+        </el-form-item>
+        <el-form-item style="text-align: center;width: 60%">
+          <el-button type="primary" @click="submit" >推送 </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data: function(){
+      return {
+        form:{
+          title:'',
+          desc:''
+        }
+      }
+    },
+    methods:{
+      submit:function(){
+        this.$message.success('信息发布成功！');
+      }
+    }
+  }
+</script>
+<style scoped>
+  .like{
+    margin-left: 20%;
+    margin-top: 5%;
+    width:50%;
+  }
+</style>
